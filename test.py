61dcc454462f841f75655a1b62b65a218de813a2
K1A2 <jckim0307@gmail.com>
2024-07-22 12:48:47 +0900

    파일명 변경

--- test.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-
-code = qrcode.make('https://qrfy.com/?utm_source=Google&utm_medium=CPC&utm_campaign=17680026409&utm_term=qr%20code%20maker&gad_source=1&gclid=Cj0KCQjwkdO0BhDxARIsANkNcrfErfu3V0ztbOAN2_YjxlhdNhLMmjzDfHouAZIx5kZNfoDHi9wHCYoaAmDlEALw_wcB', error_correction=qrcode.constants.ERROR_CORRECT_M)
-print(type(code))
-code.save("test.png")
